cff1c86bca8daf9ef5f58b843e9e254e1f28ab29
fernandog <grandefernandojavier@outlook.com>
2023-03-29 19:00:22 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>this is a title</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const note = notes.find((n) => n.id === id);
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  notes = notes.filter((n) => n.id !== id);
+  res.status(204).end();
+});
+
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  res.json(note);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`server running on port  ${PORT}`);
+});
