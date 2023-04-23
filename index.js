4011fc5a1950e07df1f13a4c720dda0c4529b1f8
Fernando <grandefernandojavier@outlook.com>
2023-04-23 04:12:50 -0300

    CORS

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 import "dotenv/config";
 import "./database/db.js";
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
@@ -10,11 +9,22 @@ import noteRouter from "./routes/routeNotes.js";
 
 const app = express();
 
+const whiteList = [process.env.ORIGIN1];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whiteList.includes(origin)) {
+        return callback(null, origin);
+      }
+      return callback(`CORS error ${origin} unauthorized `);
+    },
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1", noteRouter);
 app.use("/api/v1/auth", authRoutes);
-
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
